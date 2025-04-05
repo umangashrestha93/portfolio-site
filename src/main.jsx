8c0c2aba9ecf2a8217c52f4bce2ac6c660de3a85
umangashrestha93 <umangashrestha93@gmail.com>
2025-04-05 11:46:09 +0545

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './component/Home';
+
+const MainLayout = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </Router>
+  )
+}
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
+root.render(
+  <React.StrictMode>
+    <MainLayout />
+  </React.StrictMode>
+);
+
